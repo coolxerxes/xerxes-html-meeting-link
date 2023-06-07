178cba530ebb587103860c9bd8c6d741546a51f7
coolxerxes <124738348+coolxerxes@users.noreply.github.com>
2023-06-06 23:34:46 -0700

    red

--- models/abstract.repository.js
+++ models/abstract.repository.js
@@ -3,9 +3,10 @@ const firebaseAdmin = require('firebase-admin');
 class AbstractRepository {
   collection = null;
   bucket = null;
+  app = null;
 
   constructor(collection) {
-    const initialApp = firebaseAdmin
+    this.app = firebaseAdmin
       .initializeApp(
         {
           credential: firebaseAdmin.credential.cert({
@@ -17,10 +18,11 @@ class AbstractRepository {
         collection
       )
 
-    this.collection = initialApp
+    this.collection = this.app
       .firestore()
       .collection(collection)
 
+    this.bucket = this.app.storage().bucket('gs://connect-cbff7.appspot.com');
   }
 }
 
