31ac8c284b58d9e548853e35d4fefbcd07ff3ea9
coolxerxes <124738348+coolxerxes@users.noreply.github.com>
2023-06-06 22:10:49 -0700

    red

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/login', (req, res) => res.render('pages/login'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
